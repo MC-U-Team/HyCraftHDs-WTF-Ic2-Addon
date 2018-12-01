d767a2784204e706c4d8d4b36a04f285c4dba1f1
HyCraftHD <hycrafthd@live.de>
2018-12-01 16:53:34 +0100

    Added creative tab

---
+++ src/main/java/info/u_team/hycrafthds_wtf_ic2_addon/init/WTFIC2AddonCreativeTabs.java
@@ -0,0 +1,14 @@
+package info.u_team.hycrafthds_wtf_ic2_addon.init;
+
+import info.u_team.hycrafthds_wtf_ic2_addon.WTFIC2AddonConstants;
+import info.u_team.u_team_core.creativetab.UCreativeTab;
+
+public class WTFIC2AddonCreativeTabs {
+	
+	public static final UCreativeTab tab = new UCreativeTab(WTFIC2AddonConstants.MODID, "tab");
+	
+	public static void init() {
+		// tab.setIcon(XXX);
+	}
+	
+}
