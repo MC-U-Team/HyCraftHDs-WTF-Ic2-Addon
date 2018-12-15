4726b050b5d1f9f6134e34ef3a28ab9dc3bf2cdc
HyCraftHD <hycrafthd@live.de>
2018-12-15 14:59:07 +0100

    Dont use the jei plugin yet

--- src/main/java/info/u_team/hycrafthds_wtf_ic2_addon/jei/JeiPlugin.java
+++ src/main/java/info/u_team/hycrafthds_wtf_ic2_addon/jei/JeiPlugin.java
@@ -9,22 +9,21 @@ import mezz.jei.api.*;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 import net.minecraft.item.ItemStack;
 
-@JEIPlugin
 public class JeiPlugin implements IModPlugin {
 	
 	public static final String uuIndexID = WTFIC2AddonConstants.MODID + ".uuIndex";
 	
 	@Override
 	public void registerCategories(IRecipeCategoryRegistration registry) {
-		IGuiHelper guiHelper = registry.getJeiHelpers().getGuiHelper();
-		registry.addRecipeCategories(new RecipeCategoryUUIndex(guiHelper));
+//		IGuiHelper guiHelper = registry.getJeiHelpers().getGuiHelper();
+//		registry.addRecipeCategories(new RecipeCategoryUUIndex(guiHelper));
 	}
 	
 	@Override
 	public void register(IModRegistry registry) {
-		registry.handleRecipes(Entry.class, RecipeWrapperUUIndex::new, uuIndexID);
-		ArrayList<Entry<ItemStack, Double>> list = new ArrayList<>();
-		UuGraph.iterator().forEachRemaining(entry -> list.add(entry));
-		registry.addRecipes(list, uuIndexID);
+//		registry.handleRecipes(Entry.class, RecipeWrapperUUIndex::new, uuIndexID);
+//		ArrayList<Entry<ItemStack, Double>> list = new ArrayList<>();
+//		UuGraph.iterator().forEachRemaining(entry -> list.add(entry));
+//		registry.addRecipes(list, uuIndexID);
 	}
 }
